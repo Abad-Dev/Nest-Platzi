126fd7a411118d3b0b780883f0848c62338c061d
sedron <jfabianabadd@gmail.com>
2024-03-03 13:28:34 -0500

    Added services and types

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,20 +1,34 @@
-import { Body, Controller, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { CreateProductDto, UpdateProductDto } from 'src/dtos/Products.dto';
+import { Product } from 'src/entities/Product.entity';
+import { ProductsService } from 'src/services/products/products.service';
 
 @Controller('products')
 export class ProductsController {
+    constructor(private productsService: ProductsService) {}
+
+    @Get()
+    getProducts() {
+        return this.productsService.findAll();
+    }
+
+    @Get(':id')
+    getProduct(@Param('id', ParseIntPipe) id: number) {
+        return this.productsService.findOne(id);
+    }
+
     @Post()
-    createProduct(@Body() payload: any) {
-        return {
-            message: "Creation action",
-            body: payload
-        }
+    createProduct(@Body() payload: CreateProductDto) {
+        return this.productsService.createProduct(payload);
     }
 
     @Put(':id')
-    updateProduct(@Param('id') id: number, @Body() payload: any) {
-        return {
-            id,
-            body: payload
-        }
+    updateProduct(@Param('id') id: number, @Body() payload: UpdateProductDto) {
+        return this.productsService.updateProduct(id, payload);
+    }
+
+    @Delete(':id')
+    deleteProduct(@Param('id') id: number) {
+        return this.productsService.deleteProduct(id);
     }
 }
--- src/services/products/products.service.ts
+++ src/services/products/products.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateProductDto, UpdateProductDto } from 'src/dtos/Products.dto';
+import { Product } from 'src/entities/Product.entity';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    private products: Product[] = [
+        {
+            id: 1,
+            name: 'Product 1',
+            description: 'bla bla',
+            price: 122,
+            image: '',
+            stock: 12,
+        },
+    ];
+
+    findAll() {
+        return this.products;
+    }
+
+    findOne(id: number) {
+        const product = this.products.find((item) => item.id == id);
+        if (!product) {
+            throw new NotFoundException("Product with id: " + id + " not found or it doesn't exists.");
+        }
+        return product
+    }
+
+    createProduct(payload: CreateProductDto) {
+        this.products.push({...payload});
+        return payload;
+    }
+
+    updateProduct(id: number, payload: UpdateProductDto) {
+        const product = this.findOne(id);
+        if (!product) {
+            return false;
+        }
+        const index = this.products.findIndex(item => item.id == id);
+        this.products[index] = {...product, ...payload}
+        return this.products[index];
+    }
+
+    deleteProduct(id: number) {
+        const product = this.findOne(id);
+        if (!product) {
+            return false;
+        }
+        const index = this.products.findIndex(item => item.id == id)
+        this.products.splice(index, 1);
+        return true;
+    }
+}
